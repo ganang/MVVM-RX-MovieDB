18f0eedbc726ac900b4c680aa92080ea524f0971
Ganang Arief Pratama <ganang.arief.p@gmail.com>
2021-03-15 13:52:06 +0700

    Initial Commit

---
+++ MVVM-RXSwift-MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-RXSwift-MovieDB
+//
+//  Created by Ganang Arief Pratama on 15/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
